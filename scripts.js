2d2f133ccc3da162b02eb819782cbf04b5fb34d6
Emmanuel <superkiko80.com>
2023-05-28 23:26:42 -0600

    terminadas las funcionalidades, agregados algunos extras, sigue sin la interfez sugerida por alura

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 function encriptar(){
     const texto = document.getElementById('texto').value;
 
+    if (texto.trim() === '') {
+        alert("Por favor, inserta o teclea un texto en el área correspondiente para poder encriptar/desencriptar.");
+        return;
+    }
+
     let textoEncriptado = texto.replace(/e/g, 'enter');
     textoEncriptado = textoEncriptado.replace(/i/g, 'imes');
     textoEncriptado = textoEncriptado.replace(/a/g, 'ai');
@@ -15,6 +20,11 @@ document.getElementById('resultado').textContent = textoEncriptado;
 function desencriptar() {
     const textoEncriptado = document.getElementById('texto').value;
 
+    if (textoEncriptado.trim() === '') {
+        alert("Por favor, inserta o teclea un texto en el área correspondiente para poder encriptar/desencriptar.");
+        return;
+    }
+
     let texto = textoEncriptado.replace(/enter/g, 'e');
     texto = texto.replace(/imes/g, 'i');
     texto = texto.replace(/ai/g, 'a');
@@ -24,4 +34,30 @@ function desencriptar() {
     document.getElementById('resultado').value = texto;
   }
 
- 
+  function copiarTexto() {
+    var textarea = document.getElementById("resultado");
+      // Seleccionar el contenido del textarea
+    textarea.select();
+      // Copiar el contenido al portapapeles
+    document.execCommand("copy");
+      // Deseleccionar el contenido
+    textarea.setSelectionRange(0, 0);
+      // Mostrar una notificación o mensaje de éxito
+      textarea.value = "";
+    alert("El texto ha sido copiado al portapapeles");
+  }
+
+  function pegarTexto() {
+    var textarea = document.getElementById("texto");
+  
+    // Pegar el contenido del portapapeles en el textarea
+    navigator.clipboard.readText()
+      .then(function(text) {
+        textarea.value = text;
+        console.log("Texto pegado en el textarea: " + text);
+      })
+      .catch(function(error) {
+        console.error("Error al pegar el texto: " + error);
+      });
+  }
+  
